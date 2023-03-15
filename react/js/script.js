74259720e32fe6dbcd9443f1cac593792da8d168
Aufa Billah <aufa.jazama@tokopedia.com>
2023-03-16 05:53:51 +0700

    feat: update to react 18 in sample

--- react/js/script.js
+++ react/js/script.js
@@ -1,83 +1,21 @@
 /**
- * Membuat component Header.
- * Component Header menampilkan navigasi.
+ * Membuat Component Hello
+ * Component Hello mengemballikan/mencetak UI (elements)
  */
-function Header() {
-  return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  );
-}
-
-/**
- * Membuat component Main.
- * Component Main menampung konten utama.
- */
-function Main() {
-  return (
-    <main>
-      {/**
-       * Mengirim props ke component Hello.
-       * nama props: name
-       */}
-      <Hello name="Aufa" />
-      <Hello name="Mikel" />
-      <Hello name="Hannah" />
-      <Hello name="Jonas" />
-      <Hello name="Martha" />
-    </main>
-  );
-}
-
-/**
- * Membuat component Footer.
- * Component Footer menampilkan footer website.
- * @returns
- */
-function Footer() {
-  return (
-    <footer>
-      <h2>Copyright @aufaroot18</h2>
-      <p>Website ini dibuat menggunakan ReactJS</p>
-    </footer>
-  );
-}
-
-/**
- * Membuat component Hello.
- * Component Hello menerima inputan: props.
- *
- */
-function Hello(props) {
-  // Melakukan destructing props (object)
-  const { name } = props;
-
+function Hello() {
   return (
     <div>
       <h2>Hello React</h2>
-      <p>Saya {name} - Frontend Engineer</p>
+      <p>Saya Frontend Engineer</p>
     </div>
   );
 }
 
-/**
- * Membuat component App.
- * Component utama yang menampung components lain.
- */
-function App() {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
-}
+// Akses element yang memiliki id root
+const rootElement = document.getElementById("root");
 
-const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
+// Buat React root untuk menampilkan Component di browser
+const root = ReactDOM.createRoot(rootElement);
+
+// Render component Hello ke root
+root.render(<Hello />);
