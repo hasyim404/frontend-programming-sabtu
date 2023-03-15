006223d4b2afe77017896ff91aeedf8015e4ce8f
Aufa Billah <aufa.jazama@tokopedia.com>
2023-03-15 21:59:18 +0700

    feat: upgrade to react 18

--- movie-database/src/index.js
+++ movie-database/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/**
+ * @description: This is for learning purpose
+ * @author: Aufa Billah
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// get root element
+const container = document.getElementById("root");
+
+// create root
+const root = ReactDOM.createRoot(container);
+
+// render app to root
+root.render(<App />);
+
 reportWebVitals();
